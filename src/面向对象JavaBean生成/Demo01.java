5a4157ba0ab640b29cd92db8e1fb3245d641b474
jym <455374109@qq.com>
2022-07-27 23:23:21 +0800

    一键生成javabean

---
+++ src/面向对象JavaBean生成/Demo01.java
@@ -0,0 +1,102 @@
+package 面向对象JavaBean生成;
+
+public class Demo01 {
+    private String userName;
+    private String gender;
+    private int age;
+    private String password;
+    private String email;
+
+
+    public Demo01() {
+    }
+
+    public Demo01(String userName, String gender, int age, String password, String email) {
+        this.userName = userName;
+        this.gender = gender;
+        this.age = age;
+        this.password = password;
+        this.email = email;
+    }
+
+    /**
+     * 获取
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * 设置
+     * @param userName
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * 获取
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * 设置
+     * @param gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
